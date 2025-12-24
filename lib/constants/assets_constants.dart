d85105ed5192f3cad265ed3c3a516d9dd4d0238f
rv-bit <robertvilcu89@gmail.com>
2025-12-24 19:13:18 +0000

    chore: add logo icon

--- lib/constants/assets_constants.dart
+++ lib/constants/assets_constants.dart
@@ -1,6 +1,7 @@
 
 class AssetsConstants {
 	static const String _svgsPath = 'assets/svgs';
+	static const String logoIcon = '$_svgsPath/logo.svg';
 	static const String blogInsert = '$_svgsPath/blog_insert.svg';
 	static const String homeFilledIcon = '$_svgsPath/home_filled.svg';
 	static const String homeOutlinedIcon = '$_svgsPath/home_outlined.svg';
