270bf5086da08d999935b41a1d28df6584e94646
rv-bit <robertvilcu89@gmail.com>
2025-12-24 20:11:17 +0000

    style: remove default app bar styles

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -4,10 +4,6 @@ import 'package:flutter_blog_app/config/theme_pallet.dart';
 class AppTheme {
 	static ThemeData theme = ThemeData.dark().copyWith(
 		scaffoldBackgroundColor: Palette.backgroundColor,
-		appBarTheme: AppBarTheme(
-			backgroundColor: Palette.backgroundColor,
-			elevation: 0,
-		),
 		floatingActionButtonTheme: FloatingActionButtonThemeData(
 			backgroundColor: Palette.blueColor,
 		),
