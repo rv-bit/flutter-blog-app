09261ab0d729fd9712c3675498e703aaf8eb62a4
rv-bit <robertvilcu89@gmail.com>
2025-12-30 18:29:33 +0000

    chore: listen on logger functions and write to terminal

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,11 +9,17 @@ import 'package:flutter_blog_app/core/database/migration_utils.dart';
 
 import 'app/app.dart'; 
 
+
 void main() async {
 	WidgetsFlutterBinding.ensureInitialized();
   
 	// Set up logging
 	Logger.root.level = Level.ALL;
+	Logger.root.onRecord.listen((rec) {
+		stderr.writeln('${rec.time} [${rec.loggerName}] ${rec.level.name}: ${rec.message}');
+		if (rec.error != null) stderr.writeln('Error: ${rec.error}');
+		if (rec.stackTrace != null) stderr.writeln(rec.stackTrace);
+	});
 
 	// Check migration status (optional, for debugging)
 	await MigrationUtils.printCurrentVersion();
