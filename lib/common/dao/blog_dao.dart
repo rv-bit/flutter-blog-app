6da67964038e3ea13674e2cd47e413368aa4111c
rv-bit <robertvilcu89@gmail.com>
2025-12-30 18:34:08 +0000

    chore: refactor dao and main repository to return optimized versions of the blog rows + add image helper functions in dao

--- lib/common/dao/blog_dao.dart
+++ lib/common/dao/blog_dao.dart
@@ -12,6 +12,8 @@ class BlogDAO {
 	final databaseHelper = DatabaseHelper();
 
 	Future<void> insertBlogPost(Map<String, dynamic> blog) async {
+		if (blog.isEmpty) return;
+
 		try {
 			final database = await databaseHelper.database;
 			await database.insert(
@@ -20,17 +22,73 @@ class BlogDAO {
 				conflictAlgorithm: ConflictAlgorithm.replace,
 			);
 		} catch (e) {
+			log.severe('Insert failed: $e');
 			throw MyDatabaseException('Insert failed: $e');
 		}
 	}
 
-	// Return a List of multiple objects aka posts, and each post will include all data like title, content, image etc.
+	Future<void> insertBlogWithImages({
+		required Map<String, dynamic> blog,
+		required List<Map<String, dynamic>> images,
+	}) async {
+		if (blog.isEmpty) return;
+		if (images.isEmpty) return;
+
+		final db = await databaseHelper.database;
+
+		await db.transaction((txn) async {
+			await txn.insert(
+				'blog_posts',
+				blog,
+				conflictAlgorithm: ConflictAlgorithm.abort,
+			);
+
+			for (final image in images) {
+				await txn.insert(
+					'images',
+					image,
+					conflictAlgorithm: ConflictAlgorithm.abort,
+				);
+			}
+		});
+	}
+
+	// Return a List of multiple objects aka posts, and each post will include all data like content, image etc.
 	// TODO: #1 Will be ordered by the created time, not the updated will be later added a new function to order by selected sort type
-	Future<List<Map<String, dynamic>>> getBlogPosts() async {
+	Future<List<Map<String, dynamic>>> getBlogPosts({
+		required int limit,
+  		required int offset,
+	}) async {
 		final database = await databaseHelper.database;
 		return await database.query(
 			'blog_posts',
+			limit: limit,
+			offset: offset,
 			where: 'is_deleted = 0',
+			orderBy: 'created_at DESC',
+		);
+	}
+
+	Future<List<Map<String, dynamic>>> getImagesByBlogIds(List<String> blogIds) async {
+		if (blogIds.isEmpty) return [];
+		
+		final database = await databaseHelper.database;
+		final placeholders = List.filled(blogIds.length, '?').join(',');
+		
+		return await database.query(
+			'images',
+			where: 'blog_id IN ($placeholders)',
+			whereArgs: blogIds,
+			orderBy: 'created_at DESC',
+		);
+	}
+
+	Future<List<Map<String, dynamic>>> getImagesByBlogId(String blogId) async {
+		final database = await databaseHelper.database;
+		return await database.query(
+			'images',
+			where: 'blog_id IN ($blogId)',
+			whereArgs: [blogId],
 			orderBy: 'created_at DESC',
 		);
 	}
@@ -44,6 +102,15 @@ class BlogDAO {
 			orderBy: 'created_at DESC',
 		);
 		return res.isNotEmpty ? res.first : null;
+	}
+
+	Future<void> insertImage(Map<String, dynamic> imageMap) async {
+		final database = await databaseHelper.database;
+		await database.insert(
+			'images',
+			imageMap,
+			conflictAlgorithm: ConflictAlgorithm.replace
+		);
 	}
 
 	Future<int> updateBlog(String id, Map<String, dynamic> data) async {
--- lib/common/repositories/blog_repository.dart
+++ lib/common/repositories/blog_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_blog_app/models/images.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter_blog_app/models/index.dart';
@@ -12,17 +13,50 @@ class BlogRepository {
 
 	BlogRepository({required this.dao});
 
-	Future<List<BlogPost>> fetchBlogs() async {
-		final rows = await dao.getBlogPosts();
-		return rows.map(BlogPost.fromMap).toList();
+	Future<List<BlogPost>> fetchBlogs({required int limit, required int offset}) async {
+		final blogRows = await dao.getBlogPosts(limit: limit, offset: offset);
+
+		if (blogRows.isEmpty) return [];
+
+		final blogIds = blogRows.map((row) => row['id'] as String).toList();
+		final imageRows = await dao.getImagesByBlogIds(blogIds);
+
+		final images = imageRows.map(Images.fromMap).toList();
+		final imageMap = Images.groupByBlog(images);
+
+		return blogRows.map((blogMap) {
+			final blogId = blogMap['id'];
+
+			return BlogPost(
+				id: blogId,
+				images: imageMap[blogId] ?? const [], 
+				content: blogMap['content'], 
+				createdAt: blogMap['created_at'],
+				updatedAt: blogMap['updated_at'],
+				deletedAt: blogMap['deleted_at'],
+				isDeleted: blogMap['is_deleted'] == 1,
+			);
+		}).toList();
 	}
 
 	Future<BlogPost?> fetchBlogById(String id) async {
-		final row = await dao.getBlog(id);
-		if (row != null) {
-			return BlogPost.fromMap(row);
-		}
-		return null;
+		final blog = await dao.getBlog(id);
+		if (blog == null) return null;
+
+		final imageRows = await dao.getImagesByBlogId(id);
+		final images = imageRows.map(Images.fromMap).toList();
+
+		return BlogPost.fromMapWithImages(blog, images);
+	}
+
+	Future<void> addBlogWithImages({
+		required BlogPost blog,
+		required List<Images> images,
+	}) async {
+		await dao.insertBlogWithImages(
+			blog: blog.toMap(),
+			images: images.map((e) => e.toMap()).toList(),
+		);
 	}
 
 	Future<void> addBlog(BlogPost blog) async {
