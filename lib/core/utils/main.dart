2d332870da24f5fa4995dc53968c9363821e3adb
rv-bit <robertvilcu89@gmail.com>
2025-12-31 14:04:10 +0000

    feat: add formatPost create time / core function to formate date string

--- lib/core/utils/main.dart
+++ lib/core/utils/main.dart
@@ -7,3 +7,31 @@ int locationToIndex(BuildContext context) {
 	if (location.startsWith('/search')) return 1;
 	return 0;
 }
+
+String formatPostTime(String dateString) {
+	DateTime postDate;
+
+	try {
+		postDate = DateTime.parse(dateString);
+	} catch (e) {
+		return '';
+	}
+
+	final now = DateTime.now();
+	final difference = now.difference(postDate);
+
+	if (difference.inSeconds < 60) {
+		return '${difference.inSeconds}s';
+	} else if (difference.inMinutes < 60) {
+    	return '${difference.inMinutes}m';
+	} else if (difference.inHours < 24) {
+		return '${difference.inHours}h';
+	} else if (difference.inDays < 7) {
+		return '${difference.inDays}d';
+	} else {
+		final day = postDate.day.toString().padLeft(2, '0');
+		final month = postDate.month.toString().padLeft(2, '0');
+		final year = postDate.year.toString();
+		return '$day-$month-$year';
+	}
+}
