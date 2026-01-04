bb83ad70b24263c06031823e28fc6120fde020d1
rv-bit <robertvilcu89@gmail.com>
2026-01-04 15:57:20 +0000

    style: minor changes to default app theme

--- lib/config/theme/theme.dart
+++ lib/config/theme/theme.dart
@@ -4,6 +4,16 @@ import './theme_pallet.dart';
 
 class AppTheme {
 	static ThemeData theme = ThemeData.dark().copyWith(
+		colorScheme: ColorScheme.fromSeed(
+			brightness: Brightness.dark,
+			seedColor: Palette.whiteColor,
+
+			primary: Palette.whiteColor, 
+			onPrimary: Palette.whiteColor, 
+			
+			secondary: Palette.greyColor, 
+			onSecondary: Palette.greyColor
+		),
 		scaffoldBackgroundColor: Palette.backgroundColor,
 		floatingActionButtonTheme: FloatingActionButtonThemeData(
 			backgroundColor: Palette.blueColor,
@@ -13,5 +23,6 @@ class AppTheme {
 			selectionColor: Palette.searchBarColor,
 			selectionHandleColor: Palette.backgroundColor,
 		),
+		hintColor: Palette.greyColor.withValues(alpha: 0.8)
 	);
 }
