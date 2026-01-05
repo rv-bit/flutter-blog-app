720a6e74f85ff8666bc13ab0b7f9b295812526ee
rv-bit <robertvilcu89@gmail.com>
2026-01-05 20:54:17 +0000

    chore: removed old test file + auto format 'test_utils'

--- test/utils/test_utils.dart
+++ test/utils/test_utils.dart
@@ -3,5 +3,5 @@ import 'package:flutter_blog_app/core/database/database.dart';
 /// Returns a new instance of your DatabaseHelper for testing.
 /// You may want to mock or use an in-memory database for real tests.
 DatabaseHelper createTestDatabaseHelper() {
-  return DatabaseHelper();
+	return DatabaseHelper();
 }
